b7b3df5ae67fd3db3a18a1485ee1a22a3baf8418
JaimeVilches87 <jaimevilches87@gmail.com>
2021-05-15 13:22:02 -0400

    added start function and vars to java

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,24 @@
 var questions = []
 
+var score = 0;
+var currentQuestion = -1;
+var timeLeft = 0;
+var timer;
+
+function start() {
+
+    timeLeft = 75;
+    document.getElementById("timeLeft").innerHTML = timeLeft;
+
+    timer = setInterval(function () {
+        timerLeft--;
+        document.getElementById("timeLeft").innerHTML = timeLeft;
+
+        if (timeLeft <= 0) {
+            clearInterval(timer);
+            endGame();
+        }
+    };
+
+    next();
+}
